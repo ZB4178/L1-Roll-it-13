b2bd8370a713888d3385ade914349d42ec870b2c
Kiwizave <brockelsbyz4178@masseyhigh.school.nz>
2026-02-10 10:32:38 +1300

    Looping yes/no checker

--- C01_Yes_No_Basic.py
+++ C01_Yes_No_Basic.py
@@ -1,10 +1,18 @@
-want_instructions = input("Would you like to see the instructions? "). lower()
+while True:
 
-#check if user says yes/no
-if want_instructions == "yes" or want_instructions == "y":
-    print("you said yes")
+    want_instructions = input("Would you like to see the instructions? ").lower()
 
-elif want_instructions == "no" or want_instructions == "n":
-    print("you said no")
-else:
-    print("please type yes or no")
+    #check if user says yes/no
+    if want_instructions == "yes" or want_instructions == "y":
+        print("you said yes")
+        break
+
+    elif want_instructions == "no" or want_instructions == "n":
+        print("you said no")
+        break
+
+    else:
+        print("please type yes or no")
+    continue
+
+print("we done")
